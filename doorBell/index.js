6e4bb55fd0e31358cafbfb89bcfbf1cd4f1f7334
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-25 12:59:36 -0600

    Added contents to index.js

---
+++ doorBell/index.js
@@ -0,0 +1,16 @@
+var Gpio = require('onoff').Gpio,
+  buzzer = new Gpio(16, 'out'),
+  button = new Gpio(18, 'in', 'both');
+ 
+button.watch(function(err, value) {
+  if (err) exit();
+  buzzer.writeSync(value);
+});
+ 
+function exit() {
+  buzzer.unexport();
+  button.unexport();
+  process.exit();
+}
+ 
+process.on('SIGINT', exit);
