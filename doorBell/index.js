dbfb90f0891a94bcb4bd7396748778e6c5ccac92
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-25 13:06:11 -0600

    Added call to server

--- doorBell/index.js
+++ doorBell/index.js
@@ -2,6 +2,9 @@ var Gpio = require('onoff').Gpio,
   buzzer = new Gpio(16, 'out'),
   button = new Gpio(18, 'in', 'both');
  
+ // start the server
+var server = require('./server')(buzzer);
+ 
 button.watch(function(err, value) {
   if (err) exit();
   buzzer.writeSync(value);
