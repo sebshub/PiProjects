22616bff7bf61917b7ea5412809e2ab251aba22a
John Rucker <John.Rucker@Solar-Current.com>
2016-02-25 23:57:31 +0000

    add new code

--- alphanumericLED/index.js
+++ alphanumericLED/index.js
@@ -1,16 +1,16 @@
-var i2c = require('i2c-bus'),
-  i2c1 = i2c.openSync(1);
+console.log("Setting up i2c object");
 
+var i2c = require('i2c-bus'), i2c1 = i2c.openSync(1);
 var HT16K33_ADDR = 0x70;     // Address of HT16K33
 
-(function () {
-  // Turn on system oscillatior
-  i2c1.sendByteSync(HT16K33_ADDR, 0x21);
-  console.log("Turning on system oscillator...")
 
-  // Turn display on
-  i2c1.sendByteSync(HT16K33_ADDR, 0x81);
-  console.log("Powering up display...")
+// Turn on system oscillatior
+i2c1.sendByteSync(HT16K33_ADDR, 0x21);
+console.log("Turning on system oscillator...")
 
-  i2c1.closeSync();
-});
+// Turn display on
+i2c1.sendByteSync(HT16K33_ADDR, 0x81);
+console.log("Powering up display...")
+
+i2c1.closeSync();
+
