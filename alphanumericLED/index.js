d92c3b4b676181356a10fc8d527202199428bddc
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-25 17:38:01 -0600

    updated code to turn on LED

--- alphanumericLED/index.js
+++ alphanumericLED/index.js
@@ -1,41 +1,16 @@
 var i2c = require('i2c-bus'),
   i2c1 = i2c.openSync(1);
 
-var DS1621_ADDR = 0x48,
-  CMD_ACCESS_CONFIG = 0xac,
-  CMD_READ_TEMP = 0xaa,
-  CMD_START_CONVERT = 0xee;
-
-function toCelsius(rawTemp) {
-  var halfDegrees = ((rawTemp & 0xff) << 1) + (rawTemp >> 15);
-
-  if ((halfDegrees & 0x100) === 0) {
-    return halfDegrees / 2; // Temp +ve
-  }
-
-  return -((~halfDegrees & 0xff) / 2); // Temp -ve
-}
+var HT16K33_ADDR = 0x70;     // Address of HT16K33
 
 (function () {
-  var rawTemp;
+  // Turn on system oscillatior
+  i2c1.sendByteSync(HT16K33_ADDR, 0x21);
+  console.log("Turning on system oscillator...")
 
-  // Enter one shot mode (this is a non volatile setting)
-  i2c1.writeByteSync(DS1621_ADDR, CMD_ACCESS_CONFIG, 0x01);
-
-  // Wait while non volatile memory busy
-  while (i2c1.readByteSync(DS1621_ADDR, CMD_ACCESS_CONFIG) & 0x10) {
-  }
-
-  // Start temperature conversion
-  i2c1.sendByteSync(DS1621_ADDR, CMD_START_CONVERT);
-
-  // Wait for temperature conversion to complete
-  while ((i2c1.readByteSync(DS1621_ADDR, CMD_ACCESS_CONFIG) & 0x80) === 0) {
-  }
-
-  // Display temperature
-  rawTemp = i2c1.readWordSync(DS1621_ADDR, CMD_READ_TEMP);
-  console.log('temp: ' + toCelsius(rawTemp));
+  // Turn display on
+  i2c1.sendByteSync(HT16K33_ADDR, 0x81);
+  console.log("Powering up display...")
 
   i2c1.closeSync();
-}());
+});
