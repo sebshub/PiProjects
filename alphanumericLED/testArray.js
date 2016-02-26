d947d4570a98a74100841a5496024ab5c88de274
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-26 10:05:03 -0600

    dont need

--- alphanumericLED/testArray.js
+++
@@ -1,8 +0,0 @@
-var testArray = [
-  "A",  //will this comment cause a problem
-  "B", // not sure but dont think so
-  "C"   // last line
-  ];
-exports.getChar = function (indxNum){
-  return testArray[indxNum] 
-};
