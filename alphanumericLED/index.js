33a31f6f754225d2074fc6d987855a1f48f55b6b
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-26 08:54:45 -0600

    t

--- alphanumericLED/index.js
+++ alphanumericLED/index.js
@@ -16,6 +16,8 @@ var x = "5".charCodeAt(0);
 var charWord = fontLookup.getChar(x);
 console.log("sending 5 (ASCII " + x + ") is >" + charWord);
 i2c1.writeWordSync(HT16K33_ADDR, 0x00, charWord);
+i2c1.writeWordSync(HT16K33_ADDR, 0x02, charWord);
+
 
 i2c1.closeSync();
 
