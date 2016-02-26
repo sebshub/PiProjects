07ca22011b43b6bd24cbf7f8f2305fb2953ef0d9
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-26 11:13:19 -0600

    Added HTTP Server on Port 8088

--- alphanumericLED/index.js
+++ alphanumericLED/index.js
@@ -1,12 +1,34 @@
 console.log("index.js loading...");
 var LED = require('./alphNumDriver.js');
+var http = require('http'), port = 8088;
 
-var i = 9999
-while (i > 0){
-  //LED.prnStr(i.toString());
-  i--;
-}
+var server = http.createServer(function(request, response) {
+ 
+    if (request.url === '/trigger' && request.method == 'GET') {
+      // turn on the buzzer 
+      LED.prnStr("BANG");
+ 
+      response.writeHeader(200, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      });
+      response.write('{ "status": true }');
+      response.end();
+ 
+    } else {
+ 
+      response.writeHeader(200, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      });
+      response.write('{ "status": true }');
+      response.end();
+ 
+    }
 
+  server.listen(port);
+  console.log("Server Running on " + port + ".\nLaunch http://localhost:" + port);
+  return server;
 
 
 
