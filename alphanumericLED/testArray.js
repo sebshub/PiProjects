2692232bd2f05892ea5e4dd8df92933962778e48
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-26 07:22:38 -0600

    s

--- alphanumericLED/testArray.js
+++ alphanumericLED/testArray.js
@@ -1,4 +1,4 @@
 var testArray = ["A", "B", "C"]
-function pArray(){
-  console.log("This should show a B after arrow -->" + testArray[1])
-}
+exports.getChar = function (indxNum){
+  return testArray[indxNum] 
+};
