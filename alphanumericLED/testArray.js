12a10032fc224a995e0adec2cc96c2443c45a1b0
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-26 07:40:06 -0600

    t

--- alphanumericLED/testArray.js
+++ alphanumericLED/testArray.js
@@ -1,4 +1,8 @@
-var testArray = ["A", "B", "C"]
+var testArray = [
+  "A",  //will this comment cause a problem
+  "B", // not sure but dont think so
+  "C"   // last line
+  ];
 exports.getChar = function (indxNum){
   return testArray[indxNum] 
 };
