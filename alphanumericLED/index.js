3f80ffc517dc43cb67ab51165668e8b0d522d859
JohnRucker <JohnRucker@users.noreply.github.com>
2016-02-26 06:58:45 -0600

    t

--- alphanumericLED/index.js
+++ alphanumericLED/index.js
@@ -2,7 +2,7 @@ console.log("Setting up i2c object");
 
 var i2c = require('i2c-bus'), i2c1 = i2c.openSync(1);
 var HT16K33_ADDR = 0x70;     // Address of HT16K33
-
+src="testArray.js"
 var testArray = ["A", "B", "C"]
 
 pArray();
