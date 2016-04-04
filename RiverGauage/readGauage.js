76a94bece95d282184f1430a4bbd84967cf43524
John Rucker <JohnRucker@users.noreply.github.com>
2016-04-04 10:59:18 -0500

    Update readGauage.js

--- RiverGauage/readGauage.js
+++ RiverGauage/readGauage.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var parseString = requre("xml2js").parseString;
+var parseString = require("xml2js").parseString;
 
 console.log("Read river gauge starting up...");
 var rawXML = "";
@@ -10,7 +10,17 @@ request('http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=grfi2&output=
   if (!error && response.statusCode == 200) {
     //console.log(body) // Show the HTML for the Google homepage.
     rawXML = body;
-    console.log ("rawXML = " + rawXML);
+    // console.log ("rawXML = " + rawXML);
     
+    parseString(rawXML, function(error, result){
+      if(error){
+        console.log("Error: " + error);
+        return;
+      }
+      console.dir(JSON.stringify(result));
+    });
+    
+    
+  // lines below where here  
   }
 })
