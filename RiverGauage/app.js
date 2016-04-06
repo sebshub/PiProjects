81440c48d8b109481d8ca4315c0f913829f5cfd4
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-06 17:03:43 -0500

    updates

--- RiverGauage/app.js
+++ RiverGauage/app.js
@@ -1,26 +1,25 @@
 var request = require('request');
-// var parseString = require("xml2js").parseString;
+var parseString = require("xml2js").parseString;
 
 console.log("Read river gauge starting up...");
 var rawXML = "";
-// 
 
 var request = require('request');
 request('http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=grfi2&output=xml', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     //console.log(body) // Show the HTML for the Google homepage.
     rawXML = body;
-    console.log ("rawXML = " + rawXML);
-    /*
+    //console.log ("rawXML = " + rawXML);
+    
     parseString(rawXML, function(error, result){
       if(error){
         console.log("Error: " + error);
         return;
       }
-      console.dir(JSON.stringify(result));
+      //console.dir(JSON.stringify(result));
+      console.dir(result);
     });
-    */
-    
+        
     
   // lines below where here  
   }
