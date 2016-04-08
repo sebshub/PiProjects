bf76d5d7f008ea33ce85be7ae249d7ba4fec3130
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-08 15:51:23 -0500

    Updates to RiverGauage

--- RiverGauage/app.js
+++ RiverGauage/app.js
@@ -1,26 +1,41 @@
 var request = require('request');
 var parseString = require("xml2js").parseString;
 
-console.log("Read river gauge starting up...");
-var rawXML = "";
+console.log("Reading river gauge data from internet...");
 
 var request = require('request');
 request('http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=grfi2&output=xml', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    //console.log(body) // Show the HTML for the Google homepage.
-    rawXML = body;
-    //console.log ("rawXML = " + rawXML);
-    
-    parseString(rawXML, function(error, result){
+  if (!error && response.statusCode == 200) {   
+    parseString(body, function(error, result){
       if(error){
         console.log("Error: " + error);
         return;
       }
-      //console.dir(JSON.stringify(result));
-      console.dir(result);
+
+      // setup vars
+      var siteName = result.site.$.name
+      var siteId = result.site.$.id
+      var currentLvl = result.site.observed[0].datum[0].primary[0]._;
+      var currentLvlTime = new Date(result.site.observed[0].datum[0].valid[0]._);
+            
+      var frcst1DayLvl = result.site.forecast[0].datum[3].primary[0]._;
+      var frcst1DayTime = new Date(result.site.forecast[0].datum[3].valid[0]._);     
+      var change1Day = 12 * (Number(frcst1DayLvl) - Number(currentLvl));        // caculate the change in inches   
+
+      var frcst2DayLvl = result.site.forecast[0].datum[7].primary[0]._;
+      var frcst2DayTime = new Date(result.site.forecast[0].datum[7].valid[0]._);
+      var change2Day = 12 * (Number(frcst2DayLvl) - Number(currentLvl));        // caculate the change in inches 
+            
+      var frcst7DayLvl = result.site.forecast[0].datum[27].primary[0]._;
+      var frcst7DayTime = new Date(result.site.forecast[0].datum[27].valid[0]._);   
+      var change7Day = 12 * (Number(frcst7DayLvl) - Number(currentLvl));        // caculate the change in inches              
+      
+      console.log("Site Name = " + siteName);
+      console.log("Site ID = " + siteId);      
+      console.log("Current river level = " + currentLvl + ", time of reading = " + currentLvlTime.toLocaleTimeString());
+      console.log("24 hr forecast levl = " + frcst1DayLvl +", change = " + change1Day.toFixed(2) + " inches, forecast time: " + frcst1DayTime);
+      console.log("48 hr forecast levl = " + frcst2DayLvl +", change = " + change2Day.toFixed(2) + " inches, forecast time: " + frcst2DayTime);
+      console.log("7 day forecast levl = " + frcst7DayLvl +", change = " + change7Day.toFixed(2) + " inches, forecast time: " + frcst7DayTime);                  
     });
-        
-    
-  // lines below where here  
   }
 })
