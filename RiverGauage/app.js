35dd9555e6043ea695b1777b4761fb291cdb62a4
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-09 10:36:07 -0500

    ed

--- RiverGauage/app.js
+++ RiverGauage/app.js
@@ -49,7 +49,7 @@ function getData(){
         console.log("Sending river level to LED");
         
         var yy = Number(currentLvl);
-        var xx = String.fromCharCode(51);
+        var xx = String.fromCharCode(16);
         xx = xx + yy.toFixed(1);
         console.log("Sending " + xx);
         LED.prnStr(xx);       
