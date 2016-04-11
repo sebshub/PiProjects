09afd91b9b63a18ca6999769dd9ebe778c216126
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-11 14:11:16 -0500

    ed

--- panelMeter/app.js
+++ panelMeter/app.js
@@ -1,18 +1,17 @@
 var rpio = require('rpio');
 var arg2 = Number(process.argv[2]);
-if (arg2){console.log("called with maxCalibrated = " + arg2);}
+var arg3 = Number(process.argv[3]);
 
+if (arg2){console.log("called with maxCalibrated = " + arg2);}
+if (arg3){console.log("called with range = " + arg3);}
 
 var pin = 12;                   /* P12/GPIO18 */
 var range = 1024;               /* LEDs can quickly hit max brightness, so only use */
-var MaxPossibeRange = 1024;     /* LEDs can quickly hit max brightness, so only use */
 var clockdiv = 8;               /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
 
-var maxCalibrated = 512;
-if (arg2 > 0 & arg2 < MaxPossibeRange){
-    maxCalibrated = arg2;
-    
-}
+var maxCalibrated = 866;
+if (arg2 > 0 & arg2 < range){maxCalibrated = arg2;}
+if (arg3){range = arg3;}
 
 console.log("Setting up rpio.");
 rpio.open(pin, rpio.PWM);
