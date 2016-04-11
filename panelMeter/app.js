c8d48efb4671f9ae85a0c0863adabb42f15bebc7
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-11 12:27:58 -0500

    test

--- panelMeter/app.js
+++ panelMeter/app.js
@@ -2,7 +2,7 @@ var rpio = require('rpio');
 
 var pin = 12;           /* P12/GPIO18 */
 var range = 1024;       /* LEDs can quickly hit max brightness, so only use */
-var max = 128;          /*   the bottom 8th of a larger scale */
+var max = 256;          /*   the bottom 8th of a larger scale */
 var clockdiv = 8;       /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
 var interval = 5;       /* setInterval timer, speed of pulses */
 var times = 5;          /* How many times to pulse before exiting */
