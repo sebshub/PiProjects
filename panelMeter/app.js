7f13ee2b6490c11e18e288cddb52b14944cce219
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-11 13:42:09 -0500

    ed

--- panelMeter/app.js
+++ panelMeter/app.js
@@ -3,9 +3,10 @@ var arg2 = process.argv[2];
 if (arg2){console.log("called with maxCalibrated = " + arg2);}
 
 
-var pin = 12;           /* P12/GPIO18 */
-var MaxPossibeRange = 1024;       /* LEDs can quickly hit max brightness, so only use */
-var clockdiv = 8;       /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
+var pin = 12;                   /* P12/GPIO18 */
+var range = 1024;               /* LEDs can quickly hit max brightness, so only use */
+var MaxPossibeRange = 1024;     /* LEDs can quickly hit max brightness, so only use */
+var clockdiv = 8;               /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
 
 var maxCalibrated = 512
 if (arg2 > 0 & arg2 < MaxPossibeRange){
