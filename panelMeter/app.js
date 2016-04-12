96ba44dfb0eed330f97216726ff7093517e3e844
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-12 09:20:31 -0500

    ed

--- panelMeter/app.js
+++ panelMeter/app.js
@@ -4,20 +4,24 @@ var arg2 = Number(process.argv[2]);
 var pin = 12;           // P12/GPIO18 (50uA panel meter connected via 56k ohm resistor)
 var range = 500;        // max PWM that can be sent 
 var clockdiv = 2048;    // Clock divider (PWM refresh rate), 8 == 2.4MHz
-var mtr1Value = 0;      // Globatl value for meter 1
 
-if (arg2){
-    console.log("called with disply value = " + arg2);
-    mtr1Value = arg2;
-}
+exports.setPanelMeter = setMeter;
 
-console.log("Setting up rpio object.");
+console.log("Setting up PWM object.");
 rpio.open(pin, rpio.PWM);
 rpio.pwmSetClockDivider(clockdiv);
 rpio.pwmSetRange(pin, range);
 
+if (arg2){
+    console.log("Running in test mode. Called with " + arg2);
+    console.log("Setting needel to " + arg2 + ", PWM = " + mtr1.getCalibratedPWM(arg2));
+    setMeter(arg2);
+}
 
-console.log("Setting needel to " + mtr1Value + ", PWM = " + mtr1.getCalibratedPWM(mtr1Value));
+// Functions
+function setMeter(intVale){
+    rpio.pwmSetData(pin, mtr1.getCalibratedPWM(intVale));
+}
 
-rpio.pwmSetData(pin, mtr1.getCalibratedPWM(mtr1Value));
+
 
