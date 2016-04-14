973e2ae8a9df0effbd1ac0961f69c0bad8fd971c
JohnRucker <John.Rucker@Solar-Current.com>
2016-04-14 11:25:31 -0500

    ed

--- alphanumericLED/alphNumDriver.js
+++ alphanumericLED/alphNumDriver.js
@@ -1,6 +1,6 @@
 console.log("Setting up i2c object");
 var i2c = require('i2c-bus'), i2c1 = i2c.openSync(1);
-var HT16K33_ADDR = 0x70;     // Address of HT16K33
+var HT16K33_ADDR = 0x71;     // Address of HT16K33
 var fontLookup = require('./fontArray.js');
 
 var dsplyOnOff = 1;                         // 1 = On
